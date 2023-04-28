9304d099418177b7a3b892a25b5220716264b7e0
HughMorris01 <gfarrell2@Live.com>
2023-04-28 19:48:52 -0400

    Create Components

---
+++ src/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="sidebar-header">
+        <h3>Strapi Menu</h3>
+        <button type="button">close</button>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
