f12434a585217e81ea01b458c710fcea253589ca
HughMorris01 <gfarrell2@Live.com>
2023-04-30 22:03:20 -0400

    Save Updates

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,16 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Sidebar = () => {
+  const { closeSidebar } = useGlobalContext();
+
   return (
     <div>
       <div className="sidebar-header">
         <h3>Strapi Menu</h3>
-        <button type="button">close</button>
+        <button type="button" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
       </div>
     </div>
   );
